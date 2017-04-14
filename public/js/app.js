e9f55fb26583bd6038b8efc7d58422baf648c8c9
bobbyschank <robertschank@gmail.com>
2017-04-14 16:05:17 -0600

    initial commit

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,6 @@
  *
  */
 
-
 /* hard-coded data! */
 var sampleAlbums = [];
 sampleAlbums.push({
@@ -34,61 +33,74 @@ sampleAlbums.push({
            });
 /* end of hard-coded data */
 
-
-
-
 $(document).ready(function() {
   console.log('app.js loaded!');
 
-});
+  $.get('http://localhost:3000/api/albums', function(albums) {
+    console.log(albums);
+    albums.forEach(function(album) {
+      renderAlbum(album);
+    });
+  });
 
-
-
-
+  $("#newAlbum").submit(function(event) {
+    console.log('SUBMIT baby, YEAH!!');
+    event.preventDefault();
+    $(this).trigger("reset");
+  });
 
 // this function takes a single album and renders it to the page
-function renderAlbum(album) {
-  console.log('rendering album:', album);
+  function renderAlbum(album) {
+    console.log('rendering album:', album);
 
-  var albumHtml =
-  "        <!-- one album -->" +
-  "        <div class='row album' data-album-id='" + "HARDCODED ALBUM ID" + "'>" +
-  "          <div class='col-md-10 col-md-offset-1'>" +
-  "            <div class='panel panel-default'>" +
-  "              <div class='panel-body'>" +
-  "              <!-- begin album internal row -->" +
-  "                <div class='row'>" +
-  "                  <div class='col-md-3 col-xs-12 thumbnail album-art'>" +
-  "                     <img src='" + "http://placehold.it/400x400'" +  " alt='album image'>" +
-  "                  </div>" +
-  "                  <div class='col-md-9 col-xs-12'>" +
-  "                    <ul class='list-group'>" +
-  "                      <li class='list-group-item'>" +
-  "                        <h4 class='inline-header'>Album Name:</h4>" +
-  "                        <span class='album-name'>" + "HARDCODED ALBUM NAME" + "</span>" +
-  "                      </li>" +
-  "                      <li class='list-group-item'>" +
-  "                        <h4 class='inline-header'>Artist Name:</h4>" +
-  "                        <span class='artist-name'>" +  "HARDCODED ARTIST NAME"+ "</span>" +
-  "                      </li>" +
-  "                      <li class='list-group-item'>" +
-  "                        <h4 class='inline-header'>Released date:</h4>" +
-  "                        <span class='album-releaseDate'>" + "HARDCODED ALBUM RELEASE" + "</span>" +
-  "                      </li>" +
-  "                    </ul>" +
-  "                  </div>" +
-  "                </div>" +
-  "                <!-- end of album internal row -->" +
+    var albumHtml =
+    "        <!-- one album -->" +
+    "        <div class='row album' data-album-id='" + "HARDCODED ALBUM ID" + "'>" +
+    "          <div class='col-md-10 col-md-offset-1'>" +
+    "            <div class='panel panel-default'>" +
+    "              <div class='panel-body'>" +
+    "              <!-- begin album internal row -->" +
+    "                <div class='row'>" +
+    "                  <div class='col-md-3 col-xs-12 thumbnail album-art'>" +
+    "                     <img src='" + "http://placehold.it/400x400'" +  " alt='album image'>" +
+    "                  </div>" +
+    "                  <div class='col-md-9 col-xs-12'>" +
+    "                    <ul class='list-group'>" +
+    "                      <li class='list-group-item'>" +
+    "                        <h4 class='inline-header'>Album Name:</h4>" +
+    "                        <span class='album-name'>" + album.name + "</span>" +
+    "                      </li>" +
+    "                      <li class='list-group-item'>" +
+    "                        <h4 class='inline-header'>Artist Name:</h4>" +
+    "                        <span class='artist-name'>" + album.artistName + "</span>" +
+    "                      </li>" +
+    "                      <li class='list-group-item'>" +
+    "                        <h4 class='inline-header'>Released date:</h4>" +
+    "                        <span class='album-releaseDate'>" + album.releaseDate + "</span>" +
+    "                      </li>" +
+    "                    </ul>" +
+    "                  </div>" +
+    "                </div>" +
+    "                <!-- end of album internal row -->" +
 
-  "              </div>" + // end of panel-body
+    "              </div>" + // end of panel-body
 
-  "              <div class='panel-footer'>" +
-  "              </div>" +
+    "              <div class='panel-footer'>" +
+    "              </div>" +
 
-  "            </div>" +
-  "          </div>" +
-  "          <!-- end one album -->";
+    "            </div>" +
+    "          </div>" +
+    "          <!-- end one album -->";
 
-  // render to the page with jQuery
 
-}
+
+
+    // render to the page with jQuery
+    $("#albums").append(albumHtml);
+    // $("#albums").append( "<p>Test</p>" );
+    console.log('in renderAlbum');
+  }
+
+
+
+});
